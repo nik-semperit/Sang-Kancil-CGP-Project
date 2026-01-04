344753b545bd535021980cfe446ea5ef45db92e5
Paavan06 <paavanim4u2006@gmail.com>
2026-01-04 23:58:04 +0800

    Optimazition

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -1,28 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBar3 : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
-
     public Slider slider;
-    public Gradient gradient;
-    public Image fill;
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
-
-        fill.color = gradient.Evaluate(1f);
     }
-
-    public void SetHealth(int health)
-    {
-        slider.value = health;
-
-        fill.color = gradient.Evaluate(slider.normalizedValue);
-    }
-
 }
